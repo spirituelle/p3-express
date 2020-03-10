19293f0723c33cca0b0a6436a2226531d19fadc3
unknown <nwalalfadly8@gmail.com>
2020-03-10 09:09:41 +0100

    mise à jours

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 //import routes
 
 // const users = require('./routes/user');
-// const posts = require('./routes/post');
+const post = require('./routes/posts');
 // const comments = require('./routes/comment');
 // const categories = require('./routes/category');
 // const types = require('./routes/type');
@@ -26,13 +26,13 @@ const Tag = require('./models/tag');
 const app = express();
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
 // app.use('/user',users)
-// app.use('/post',posts)
+app.use('/posts',post)
 // app.use('/comment',comments)
 // app.use('/category',categories)
 // app.use('/type',types)
--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,11 @@
-// dksdjh
+const express = require('express');
+const router = express.Router();
+const PostController = require('./../controllers/PostController')
+
+router.get('', PostController.getAllPost)
+router.get('/:id', PostController.getOnePost)
+router.post('', PostController.addPost)
+router.put('/:id', PostController.updatePost)
+router.delete('/:id', PostController.deletePost)
+router.patch('/:id', PostController.updateOneElementPost)
+module.exports = router;
